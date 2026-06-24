441abb28f0796507122687b436a1765bc45db07c
manan.prajapati <mananprajapati2805@gmail.com>
2026-06-24 16:03:30 +0530

    removed comment

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// Login page component for TodoSphere.
+
 import { Link, useNavigate } from "react-router-dom";
 import { CheckSquare, AlertCircle } from "lucide-react";
 
